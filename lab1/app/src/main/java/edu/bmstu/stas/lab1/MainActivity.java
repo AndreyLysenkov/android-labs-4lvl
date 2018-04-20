25fbe16c25511b3b75275c6be5c645e24d1284e3
Andrey Lysenkov <lysenkov-ae@ya.ru>
2018-04-20 17:01:14 +0300

    1st lab seems to be done

--- lab1/app/src/main/java/edu/bmstu/stas/lab1/MainActivity.java
+++ lab1/app/src/main/java/edu/bmstu/stas/lab1/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle(android.os.Build.MODEL);
         setContentView(R.layout.activity_main);
     }
 }
