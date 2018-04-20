a1946a5ee60bad4a9c42a6ef35c108375443bbb9
Andrey Lysenkov <lysenkov-ae@ya.ru>
2018-04-20 20:32:51 +0300

    add change color method lab3

--- lab2/app/src/main/java/edu/bmstu/stas/lab2/ActivityFigure.java
+++ lab2/app/src/main/java/edu/bmstu/stas/lab2/ActivityFigure.java
@@ -1,5 +1,9 @@
 package edu.bmstu.stas.lab2;
 
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.widget.ImageView;
+
 public class ActivityFigure {
 
     public enum eColor {
@@ -33,5 +37,24 @@ public class ActivityFigure {
         this.Form = eForm.CIRCLE;
         this.Color = eColor.BLACK;
         this.Position = ePosition.CENTER;
+    }
+
+    public void setFigureColor(ImageView view) {
+        int color = android.graphics.Color.BLACK;
+        switch (this.Color) {
+            case BLACK:
+                color = android.graphics.Color.BLACK;
+                break;
+            case GRAY:
+                color = android.graphics.Color.GRAY;
+                break;
+            case BLUE:
+                color = android.graphics.Color.BLUE;
+                break;
+            case RED:
+                color = android.graphics.Color.RED;
+                break;
+        }
+        view.setColorFilter(color);
     }
 }
