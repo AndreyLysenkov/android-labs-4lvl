26bdfcee77bd98be841f6064096cc6d6fee0bd15
Andrey Lysenkov <lysenkov-ae@ya.ru>
2018-04-20 18:27:21 +0300

    add example menu

--- lab2/app/src/main/java/edu/bmstu/stas/lab2/MainActivity.java
+++ lab2/app/src/main/java/edu/bmstu/stas/lab2/MainActivity.java
@@ -2,6 +2,9 @@ package edu.bmstu.stas.lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.Menu;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_selection, menu);
+        return true;
+    }
+
 }
