c98c11b355b192c3884ac1ec967b28881953d91c
Andrey Lysenkov <lysenkov-ae@ya.ru>
2018-04-22 16:53:46 +0300

    add output activity

--- lab5/app/src/main/java/edu/bmstu/stas/lab5/MainActivity.java
+++ lab5/app/src/main/java/edu/bmstu/stas/lab5/MainActivity.java
@@ -1,13 +1,32 @@
 package edu.bmstu.stas.lab5;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO;
+    }
+
+
+    public void onApply(View view) {
+        // TODO;
+
+        Intent intent = new Intent(MainActivity.this, OutputActivity.class);
+
+        intent.putExtra("content", ((EditText)findViewById(R.id.activity_main_content)).getText().toString());
+        intent.putExtra("color", Color.BLUE);
+        intent.putExtra("size", 20);
+
+        startActivity(intent);
     }
 }
---
+++ lab5/app/src/main/java/edu/bmstu/stas/lab5/OutputActivity.java
@@ -0,0 +1,35 @@
+package edu.bmstu.stas.lab5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class OutputActivity extends AppCompatActivity {
+
+    String content;
+    int color;
+    int size;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_output);
+
+        Bundle extras = getIntent().getExtras();
+
+        this.content = extras.getString("content");
+        this.size = extras.getInt("size");
+        this.color = extras.getInt("color");
+
+        this.setView();
+    }
+
+    private void setView() {
+        // TODO;
+        TextView view = findViewById(R.id.activity_output_content);
+
+        view.setText(this.content);
+        view.setTextSize(this.size);
+        view.setTextColor(this.color);
+    }
+}
