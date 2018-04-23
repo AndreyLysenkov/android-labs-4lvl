a0f29d77a7a653ed694649ee78ecb02676581380
Andrey Lysenkov <lysenkov-ae@ya.ru>
2018-04-23 18:18:54 +0300

    doing smth

--- lab6/client/app/src/main/java/edu/bmstu/stas/lab6/client/RecordActivity.java
+++ lab6/client/app/src/main/java/edu/bmstu/stas/lab6/client/RecordActivity.java
@@ -1,16 +1,18 @@
 package edu.bmstu.stas.lab6.client;
 
-import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RecordActivity extends AppCompatActivity
-    implements EditDialog.Listener {
+public class RecordActivity extends AppCompatActivity {
 
     public enum Mode {
         ADD,
@@ -19,8 +21,6 @@ public class RecordActivity extends AppCompatActivity
     }
 
     private Mode mode;
-
-    private int editRecordId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,61 +39,56 @@ public class RecordActivity extends AppCompatActivity
         view.setVisibility(View.GONE);
     }
 
-    private void makeVisible(int id) {
-        View view = findViewById(id);
-        view.setVisibility(View.VISIBLE);
-    }
-
     public void applyMode() {
+        int mode = R.string.activity_record_mode_label_placeholder;
         switch (this.mode) {
             case DELETE:
+                mode = R.string.activity_record_mode_label_delete;
                 makeInvisible(R.id.activity_record_form_manufacture);
                 makeInvisible(R.id.activity_record_form_model);
                 makeInvisible(R.id.activity_record_form_year);
                 makeInvisible(R.id.activity_record_field_id_fixed);
                 break;
             case EDIT:
+                mode = R.string.activity_record_mode_label_edit;
                 this.makeEditDialog();
+                // break; not
             case ADD:
-                makeInvisible(R.id.activity_record_field_id_editable);
+                mode = R.string.activity_record_mode_label_add;
+                makeInvisible(R.id.activity_record_field_id_editable); // TODO; placeholders to add;
                 break;
         }
+
+        TextView text = findViewById(R.id.activity_record_mode_label);
+        text.setText(mode);
+    }
+
+    public void setEditRecordId(String id) {
+        TextView view = findViewById(R.id.activity_record_field_id_fixed);
+        view.setText(id);
     }
 
     // https://developer.android.com/guide/topics/ui/dialogs.html
     public void makeEditDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(RecordActivity.this);
 
-        LayoutInflater inflater = RecordActivity.this.getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.activity_record_edit_dialog, null))
-                .setMessage(R.string.activity_record_field_id_editable_placeholder) // TODO;
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View view = inflater.inflate(R.layout.activity_record_edit_dialog, null, false);
+
+
+        final EditText input = (EditText) view.findViewById(R.id.activity_record_dialog_edit_input);
+
+        builder.setView(view);
+        builder.setMessage(R.string.activity_record_field_id_editable_placeholder) // TODO;
                 .setTitle(R.string.activity_record_field_id_editable_placeholder) // TODO;
                 .setPositiveButton(R.string.app_name, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                EditText view = findViewById(R.id.activity_record_dialog_edit); // TODO;
-                editRecordId = Integer.getInteger(view.getText().toString());
+                RecordActivity.this.setEditRecordId(input.getText().toString());
             }
         });
 
-        AlertDialog dialog = builder.create();
-
-        dialog.show();
+        builder.create().show();
     }
-
-    public void showEditDialog() {
-        DialogFragment dialog = new EditDialog();
-        dialog.show(getFragmentManager(), "edit_dialog");
-    }
-
-    public void onEditDialogAccept(DialogFragment dialog) {
-        // TODO;
-
-    }
-
-    public void onEditDialogCancel(DialogFragment dialog) {
-        // TODO;
-    }
-
 
     public void onAccept(View view) {
         // TODO;
